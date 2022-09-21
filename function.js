0d591e286c9f0a746acac2cc21bf86078dbf4b57
wynicius <wynicius@gmail.com>
2022-09-21 11:23:25 -0300

    Update function.js

--- function.js
+++ function.js
@@ -1,13 +1,16 @@
 function Converter() {
-  var valorElemento = document.getElementById("valor"); // valor é o id do <input>
-  var valor = valorElemento.value; // aqui se usa o .value para que retorne apenar o valor inserido no input, que vem sempre em texto POR PADRÃO!
-  var valorEmReal = parseFloat(valor); // transformação do valor de string para float
-  
-  var valorEmDolar = (valorEmReal * 5.26).toFixed(2) // a conversão em dolar
+  /* 
+  * var valorElemento = document.getElementById("valor"); // valor é o id do <input>
+  * var valor = valorElemento.value; // aqui se usa o .value para que retorne apenar o valor inserido no input, que vem sempre em texto POR PADRÃO!
+  * var valorEmReal = parseFloat(valor); // transformação do valor de string para float
+  * FIZ A TROCA DAS TRÊS LINHAS POR APENAS UMA!*/
+  var valorEmReal = parseFloat (document.getElementById("valor") .value)
 
+  var valorEmDolar = (valorEmReal * 5.26).toFixed(2) // a conversão em dolar
   
   var elementoValorConvertido = document.getElementById("valorConvertido");
-  var valorConvertido = "O valor em reais é R$" + valorEmDolar;
+  var valorConvertido = "O valor em Real é de R$" + valorEmDolar;
+  
   elementoValorConvertido.innerHTML = valorConvertido;
   event.preventDefault()
 }
